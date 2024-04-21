13e49b93909ffc9b52f9c4ddc8abe8c6c63519ee
Inga Schöyen <93376973+ingaschoeyen@users.noreply.github.com>
2024-04-21 14:49:55 +0200

    Created Pt2.py

---
+++ Ass1/scrips/Pt2.py
@@ -0,0 +1,13 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from basic import GAN
+
+
+def main(plot_path):
+  pass
+
+if __name__ == "__main__":
+  path = os.path.dirname(os.path.abspath(__file__))
+  plot_path = os.path.dirname(path) + "/plots/"
+  main(plot_path)
